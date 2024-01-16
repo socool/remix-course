81c3994e8f862e521fd3a3881cbe8cc33b8d6625
So Cool <127.0.0.1.pm@gmail.com>
2024-01-16 17:54:19 +0700

    implement action:add

--- app/routes/notes.jsx
+++ app/routes/notes.jsx
@@ -1,4 +1,6 @@
 import NewNote, { links as newNoteLinks } from "~/components/NewNote";
+import { getStoredNotes, storedNotes } from "~/data/notes";
+import { redirect } from "@remix-run/node";
 
 export default function NotesPage() {
   return (
@@ -6,6 +8,17 @@ export default function NotesPage() {
       <NewNote />
     </main>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  // Add Validation...
+  const existingNotes = await getStoredNotes();
+  noteData.id = new Date().toISOString();
+  const updatedNotes = existingNotes.concat(noteData);
+  await storedNotes(updatedNotes);
+  return redirect("/notes");
 }
 
 export function links() {
