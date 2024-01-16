2a6dabd39cf156f4cc58c371d952f2d93bc1a843
So Cool <127.0.0.1.pm@gmail.com>
2024-01-16 14:54:41 +0700

    add notes

--- app/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Link } from "@remix-run/react";
-export default function Index() {
-  return (
-    <>
-      <h1>Hello World!</h1>
-      <Link to="/demo">Go to Demo Page</Link>
-    </>
-  );
-}
---
+++ app/routes/notes.jsx
@@ -0,0 +1,7 @@
+export default function NotesPage() {
+  return (
+    <main>
+      <h1>Notes</h1>
+    </main>
+  );
+}
